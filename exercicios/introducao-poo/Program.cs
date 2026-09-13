10edb934744ef0da21f905803bd038201a9cf909
Ruhan Alves <ruhanatcm@gmail.com>
2026-09-12 22:31:06 -0300

    Update comments and formatting in Program.cs

--- exercicios/introducao-poo/Program.cs
+++ exercicios/introducao-poo/Program.cs
@@ -11,7 +11,7 @@ namespace Att1_Etapa3_POO
     {
         static void Main(string[] args)
         {
-            // 12600873 
+            // ######## 
             // Ruhan Alves Teixeira Costa Madeira
             // 2E1
 
